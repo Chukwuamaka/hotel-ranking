5a8cf4b88440424f9548e6cc1e0ce63dd4bda275
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-11-30 13:21:49 +0100

    Add filter functionality to the list

--- components/home/HotelList.tsx
+++ components/home/HotelList.tsx
@@ -2,9 +2,16 @@ import { FC, useEffect, useState } from 'react';
 import { Box, GridItem, SimpleGrid, Text } from '@chakra-ui/react';
 import HotelCard from './HotelCard';
 import { Hotel, HotelListProps } from '../../types/components/home/hotel_list';
+import Filter from './Filter';
+import { FilterProps } from '../../types/components/home/filter';
 
-const HotelList: FC<HotelListProps> = ({ query }) => {
+const HotelList: FC<HotelListProps> = ({ searchQuery }) => {
   const [hotels, setHotels] = useState<Hotel[]>([]);
+  const [filterQueries, setFilterQueries] = useState<string[]>([]);
+  
+  const applyFilter: FilterProps['applyFilter'] = (queries) => {
+    setFilterQueries(queries);
+  }
 
   useEffect(() => {
     const hotelList = localStorage.getItem('hotelList');
@@ -26,14 +33,22 @@ const HotelList: FC<HotelListProps> = ({ query }) => {
           You have not added any hotels to your directory yet. Add a hotel to see it here
         </Text>
       :
-        <SimpleGrid gap={5} columns={{base: 1, md: 2, lg: 3}} placeItems='center' className='responsive_1440px'>
-          {hotels.filter(hotel => hotel.name.toLowerCase().includes(query.toLowerCase()))
-          .map(hotel => (
-            <GridItem key={hotel.id}>
-              <HotelCard data={hotel} />
-            </GridItem>
-          ))}
-        </SimpleGrid>
+        <>
+          <Box textAlign='right' mb={10}>
+            <Filter initialQueries={filterQueries} applyFilter={applyFilter} />
+          </Box>
+          <SimpleGrid gap={5} columns={{base: 1, md: 2, lg: 3}} placeItems='center' className='responsive_1440px'>
+            {hotels.filter(hotel => (
+              filterQueries.length > 0 ? filterQueries.includes(hotel.brand) : hotel
+            ))
+            .filter(hotel => hotel.name.toLowerCase().includes(searchQuery.toLowerCase()))
+            .map(hotel => (
+              <GridItem key={hotel.id}>
+                <HotelCard data={hotel} />
+              </GridItem>
+            ))}
+          </SimpleGrid>
+        </>
       }
     </Box>
   )
